ba170dcab80f8a96e484a76df0090d0a92ce3c26
오세훈 <wbsldjgktka@naver.com>
2023-03-10 22:47:38 +0900

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-window.onload = () =>{ 
-    
+window.onload = () =>{
+
+    var swiper = new Swiper(".mySwiper", {
+      effect: "flip",
+      grabCursor: true,
+      loop: true,
+      speed: 2000,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+
 }
